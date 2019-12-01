868084903f02c84aa4b6361af0f3972767856c73
shilin <3223826042@qq.com>
2019-12-01 17:38:18 +0800

    first commit

--- php/student.php
+++ php/student.php
@@ -70,19 +70,14 @@
             $username=$_POST['username'];
             $password=$_POST['password'];//密码
             $conn = sqlsrv_connect($servername, $connectionOptions);
-            $tsql= "SELECT * FROM section where username = '$username'";
+            $tsql= "SELECT * FROM sectio";
             $getResults= sqlsrv_query($conn, $tsql);
 
             if ($getResults == FALSE)
                 echo (sqlsrv_errors());
             while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-                if (trim($row['password'])===$password) {//对比密码看是否正确
-                    setcookie("username",$row['username']);
-                    echo "1";
-                    return;
-                }
+                    echo $row["course_id"];
             }
-            echo "0";
             sqlsrv_free_stmt($getResults);
             ?>
         </div>

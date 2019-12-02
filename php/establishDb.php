0c1cf04f0113a3c1862be52267210c0bbf0404d2
shilin <3223826042@qq.com>
2019-12-02 19:13:28 +0800

    first commit

--- php/establishDb.php
+++ php/establishDb.php
@@ -13,13 +13,10 @@ $connectionOptions = array(
 );
 $conn = sqlsrv_connect($servername, $connectionOptions);
 $deptSql = "create table department
-<<<<<<< HEAD
             (dept_name nvarchar (50),
             campus nvarchar (20) not null ,
-=======
             (dept_name varchar (50),
             campus varchar (20) not null ,
->>>>>>> fe08ea4a6ada23796eac24eab2dc7cc3df64804c
             primary key (dept_name))";
 sqlsrv_query($conn,$deptSql);
 $userSql = "create table users
@@ -29,53 +26,41 @@ $userSql = "create table users
 sqlsrv_query($conn,$userSql);
 $studentSql = "create table student
               (student_id character(4),
-<<<<<<< HEAD
               student_name nvarchar(40) not null ,
               gender varchar(2) not null ,
               dept_name nvarchar(50),
-=======
               student_name varchar(40) not null ,
               gender varchar(2) not null ,
               dept_name varchar(50),
->>>>>>> fe08ea4a6ada23796eac24eab2dc7cc3df64804c
               total_credit numeric(4,1) default 0,
               primary key (student_id),
               foreign key (dept_name) references department)";
 sqlsrv_query($conn,$studentSql);
 $instructorSql = "create table instructor
                   (instructor_id character(4),
-<<<<<<< HEAD
                   instructor_name nvarchar(40) not null ,
                   title nvarchar(20),
                   dept_name nvarchar(50),
-=======
                   instructor_name varchar(40) not null ,
                   title varchar(20),
                   dept_name varchar(50),
->>>>>>> fe08ea4a6ada23796eac24eab2dc7cc3df64804c
                   primary key (instructor_id),
                   foreign key (dept_name) references department)";
 sqlsrv_query($conn,$instructorSql);
 $courseSql = "create table course
               (course_id character(4),
-<<<<<<< HEAD
               course_name nvarchar(50) not null ,
               dept_name nvarchar(50),
-=======
               course_name varchar(50) not null ,
               dept_name varchar(50),
->>>>>>> fe08ea4a6ada23796eac24eab2dc7cc3df64804c
               credit numeric(3,1) not null ,
               primary key (course_id),
               foreign key (dept_name) references department)";
 sqlsrv_query($conn,$courseSql);
 $classroomSql = "create table classroom
                 (room_no varchar(4),
-<<<<<<< HEAD
                 building nvarchar(20),
-=======
                 building varchar(20),
->>>>>>> fe08ea4a6ada23796eac24eab2dc7cc3df64804c
                 room_capacity numeric(3,0) not null ,
                 primary key (room_no,building))";
 sqlsrv_query($conn,$classroomSql);
@@ -92,11 +77,8 @@ $sectionSql = "create table section
                semester varchar(6),
                year numeric(4,0),
                room_no varchar(4),
-<<<<<<< HEAD
                building nvarchar(20),
-=======
                building varchar(20),
->>>>>>> fe08ea4a6ada23796eac24eab2dc7cc3df64804c
                time_slot_id character(4),
                take_num numeric(3,0) default 0,
                sec_capacity numeric(3,0) not null ,
@@ -126,22 +108,16 @@ $teachesSql = "create table teaches
 sqlsrv_query($conn,$teachesSql);
 $examSql = "create table exam
             (exam_id character(4),
-<<<<<<< HEAD
             exam_name nvarchar(8) not null ,
-=======
             exam_name varchar(8) not null ,
->>>>>>> fe08ea4a6ada23796eac24eab2dc7cc3df64804c
             section_id character(4),
             course_id character(4),
             semester varchar(6),
             year numeric(4,0),
             time_slot_id character(4),
             room_no varchar(4),
-<<<<<<< HEAD
             building nvarchar(20),
-=======
             building varchar(20),
->>>>>>> fe08ea4a6ada23796eac24eab2dc7cc3df64804c
             primary key (exam_id),
             foreign key (section_id,course_id,semester,year) references section,
             foreign key (room_no,building) references classroom)";
@@ -154,20 +130,16 @@ $takeApplySql = "create table take_apply
                 course_id character(4),
                 semester varchar(6),
                 year numeric(4,0),
-<<<<<<< HEAD
                 content nvarchar(400),
                 apply_time timestamp not null ,
                 state nvarchar(8) not null ,
-=======
                 content varchar(400),
                 apply_time timestamp not null ,
                 state varchar(8) not null ,
->>>>>>> fe08ea4a6ada23796eac24eab2dc7cc3df64804c
                 primary key (apply_id),
                 foreign key (student_id) references student,
                 foreign key (instructor_id) references instructor,
                 foreign key (section_id,course_id,semester,year) references section)";
-<<<<<<< HEAD
 sqlsrv_query($conn,$takeApplySql);
 $dropSecSql = "create table dropSec
         (student_id character(4),
@@ -178,6 +150,4 @@ $dropSecSql = "create table dropSec
         primary key (student_id,section_id,course_id,semester,year),
         foreign key (section_id,course_id,semester,year) references section)";
 sqlsrv_query($conn,$dropSecSql);
-=======
 sqlsrv_query($conn,$takeApplySql);
->>>>>>> fe08ea4a6ada23796eac24eab2dc7cc3df64804c
